e7e1a94d174b386cbf17073874dbe7fd1e5573b2
Matt Buland <gik0geck0@gmail.com>
2025-10-16 22:02:39 -0600

    Set pages user location

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourusername.github.io',
+  site: 'https://gik0geck0.github.io',
   base: '/astro-tools',
   integrations: [preact()]
 });
